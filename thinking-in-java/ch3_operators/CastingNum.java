2e5f6fbf363c9ffce5eb2fba69216e516d9269a7
389498623@qq.com <389498623@qq.com>
2020-04-17 17:43:16 +0800

    -

---
+++ thinking-in-java/ch3_operators/CastingNum.java
@@ -0,0 +1,17 @@
+
+public class CastingNum {
+    public static void main(String[] args) {
+        double a = 0.7;
+        double b = 3.4;
+        double c = 3.5;
+        System.out.println("a = " + a);
+        System.out.println("(int)a = " + (int)a);
+        System.out.println("(Math.round)a = " + Math.round(a));
+        System.out.println("b = " + b);
+        System.out.println("(int)b = " + (int)b);
+        System.out.println("(Math.round)b = " + Math.round(b));
+        System.out.println("c = " + c);
+        System.out.println("(int)c = " + (int)c);
+        System.out.println("(Math.round)c = " + Math.round(c));
+    }
+
